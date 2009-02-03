553da86c3f2653d0be964158512fd389d2c7d52c
U-VS2008\Len Smith <Len Smith@vs2008.(none)>
2009-02-02 20:15:11 -0800

    Pair Request Binder binds Name and Description

--- Tribality.Tests/Binders/BindingBaseTest.cs
+++ Tribality.Tests/Binders/BindingBaseTest.cs
@@ -1,32 +1,11 @@
-using System;
-using System.Web.Mvc;
 using System.Collections.Specialized;
-using Moq;
-using System.Web;
-using System.Web.Routing;
-using System.Security.Principal;
+using Moq.Mvc;
 
 namespace Tribality.Tests.Binders
 {
-    public class BindingBaseTest : BaseTest
+    public abstract class BindingBaseTest : BaseTest
     {
-
-        protected ControllerContext getControllerContext(NameValueCollection form, string userName)
-        {
-            Mock<HttpRequestBase> mockRequest = new Mock<HttpRequestBase>();
-            mockRequest.Expect(r => r.Form).Returns(form);
-
-            Mock<HttpContextBase> mockHttpContext = new Mock<HttpContextBase>();
-            mockHttpContext.Expect(c => c.Request).Returns(mockRequest.Object);
-
-            Mock<IPrincipal> principal = new Mock<IPrincipal>();
-            Mock<IIdentity> identity = new Mock<IIdentity>();
-            identity.Expect(e => e.Name).Returns(userName);
-            principal.Expect(p => p.Identity).Returns(identity.Object);
-            mockHttpContext.Expect(c => c.User).Returns(principal.Object);
-            return new ControllerContext(mockHttpContext.Object, new RouteData(), new Mock<ControllerBase>().Object);
-        }
-
-
+        protected HttpContextMock context = new Moq.Mvc.HttpContextMock();
+        protected NameValueCollection form = new NameValueCollection();
     }
 }
--- Tribality.Tests/Binders/CommentBinderSpecs/CommentBinder_when_binding_a_comment.cs
+++ Tribality.Tests/Binders/CommentBinderSpecs/CommentBinder_when_binding_a_comment.cs
@@ -1,24 +1,21 @@
-using System;
 using System.Web.Mvc;
 using System.Web.Routing;
 using Tribality.Controllers;
 using Tribality.Models.Binders;
 using NUnit.Framework;
-using System.Collections.Specialized;
 using Tribality.Models;
 using Tribality.Services;
 using Tribality.Repository;
 using Moq;
 
-namespace Tribality.Tests.Binders
+namespace Tribality.Tests.Binders.CommentBinderSpecs
 {
     [TestFixture]
     public class when_binding_a_comment : BindingBaseTest
     {
         private const string body = "Hello there.";
         private CommentBinder binder;
-        
-        protected NameValueCollection form = new NameValueCollection();
+
         readonly User firstUser = Container.Resolve<IUserRepository>().GetFirst();
         readonly BlogPost firstBlogPost = Container.Resolve<IBlogPostRepository>().GetFirst();
 
@@ -41,12 +38,12 @@ namespace Tribality.Tests.Binders
 
         [Test]
         public void a_proper_comment_object_is_returned()
-        {
-            var context = new Moq.Mvc.HttpContextMock();
+        {            
             context.HttpRequest.Expect(r => r.Form).Returns(form);
             context.Expect(c => c.User.Identity.Name).Returns("Soundwave");
-            var result = binder.BindModel(new ControllerContext(context.Object, new RouteData(), new PostController()), new ModelBindingContext());
-            Comment comment = (Comment)result;
+            var result = binder.BindModel(new ControllerContext(context.Object, new RouteData(), 
+                new PostController()), new ModelBindingContext());
+            var comment = (Comment)result;
             comment.Body.ShouldEqual(body);
         }
     }
